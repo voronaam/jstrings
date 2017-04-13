a6c3f0c9a47b0170a5d166440ee94e018506b421
Aleksey Vorona <voronaam@gmail.com>
2017-04-13 00:07:41 -0700

    Extract constant strings from java class files

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+/*
+ * A simple program that aims to extract various string constants from a JAR file (Java)
+ */
+
+extern crate classreader;
+extern crate zip;
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+use classreader::*;
+use std::fs::File;
+
+const USAGE: &'static str = "
+Java Strings extractor.
+
+Usage:
+  jstrings <source>...
+  javaminer (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+
+Source can be one or more class or jar files.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+	arg_source: Vec<String>
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    for f in args.arg_source {
+        if f.ends_with(".class") {
+            process_class_file(&f);
+        } else if f.ends_with(".jar") {
+            process_jar_file(&f);
+        } else {
+            println!("Ignoring unknown file type {}", f);
+        }
+    }
+}
+
+fn process_jar_file(file_name: &String) {
+    let file = File::open(file_name).expect("couldn't find a file!");
+    let mut zip = zip::ZipArchive::new(file).expect("could not read JAR");
+    for i in 0..zip.len() {
+        let mut class_file = zip.by_index(i).unwrap();
+        if class_file.name().ends_with("class") {
+            let class = ClassReader::new_from_reader(&mut class_file).unwrap();
+            process_class(&class);
+        }
+    }
+}
+
+fn process_class_file(file_name: &String) {
+    let class = ClassReader::new_from_path(&file_name).unwrap();
+    process_class(&class);
+}
+
+fn process_class(class: &Class) {
+    assert_eq!(0xCAFEBABE, class.magic);
+    for jstr in &class.constant_pool {
+		match jstr {
+			&ConstantPoolInfo::String(index) => {
+				println!("{}", get_string(&class, index as usize));
+			},
+			_ => {}
+		}
+	}
+}
+
+/// Get constant from a pool, correcting for java's 1-based indexes.
+fn get_const(class: &Class, i: usize) -> &ConstantPoolInfo {
+    &class.constant_pool[i - 1]
+}
+
+/// Get string from constant pool
+fn get_string(class: &Class, index: usize) -> String {
+    match get_const(class, index) {
+        &ConstantPoolInfo::Utf8(ref s) => s.clone(),
+        _ => "?".to_string()
+    }
+}
